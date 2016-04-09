c6b2bbf81631667b48ed25a7a15f04e7649a9628
Nathan Skerl <nskerl@gmail.com>
2016-04-08 19:42:58 -0700

    Update pirate_speak.js

--- assets/javascripts/pirate_speak.js
+++ assets/javascripts/pirate_speak.js
@@ -1,5 +1,8 @@
 Discourse.Dialect.postProcessText(function (text) {
   text = [].concat(text);
+  console.log('----------');
   console.log(text);
+  console.log('----------');
+  
   return text;
 });
